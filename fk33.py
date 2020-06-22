80410282b2d0cb30a65cb67c0276df93bc1fc6b9
Jędrzej Boczar <jboczar@antmicro.com>
2020-06-22 17:00:25 +0200

    add converter from AXILite to AXI

--- fk33.py
+++ fk33.py
@@ -18,9 +18,68 @@ from litex.soc.integration.builder import *
 from litex.soc.integration.soc import SoCRegion
 from litex.soc.cores.led import LedChaser
 from litex.soc.interconnect import wishbone, axi
+from litex.soc.interconnect.axi import AXIInterface, AXILiteInterface
 
 from hbm_ip import HBMIP, BusCSRDebug
 import wb2axi
+
+
+class AXILite2AXI(Module):
+    def __init__(self, axi_lite, axi, write_id=0, read_id=0, burst_type='FIXED'):
+        assert isinstance(axi_lite, AXILiteInterface)
+        assert isinstance(axi, AXIInterface)
+        assert axi_lite.data_width == axi.data_width
+        assert axi_lite.address_width == axi.address_width
+
+        burst_size = log2_int(axi.data_width // 8)
+        # burst type has no meaning as we use burst length of 1, but the AXI slave may requires
+        # certain type of burst
+        burst_type = {
+            'FIXED': 0b00,
+            'INCR': 0b01,
+            'WRAP': 0b10,
+        }[burst_type]
+
+        self.comb += [
+            axi.aw.valid.eq(axi_lite.aw.valid),
+            axi_lite.aw.ready.eq(axi.aw.ready),
+            axi.aw.addr.eq(axi_lite.aw.addr),
+            axi.aw.burst.eq(burst_type),
+            axi.aw.len.eq(0),
+            axi.aw.size.eq(burst_size),
+            axi.aw.lock.eq(0),
+            axi.aw.prot.eq(0),
+            axi.aw.cache.eq(0b0011),
+            axi.aw.qos.eq(0),
+            axi.aw.id.eq(write_id),
+
+            axi.w.valid.eq(axi_lite.w.valid),
+            axi_lite.w.ready.eq(axi.w.ready),
+            axi.w.data.eq(axi_lite.w.data),
+            axi.w.strb.eq(axi_lite.w.strb),
+            axi.w.last.eq(1),
+
+            axi_lite.b.valid.eq(axi.b.valid),
+            axi_lite.b.resp.eq(axi.b.resp),
+            axi.b.ready.eq(axi_lite.b.ready),
+
+            axi.ar.valid.eq(axi_lite.ar.valid),
+            axi_lite.ar.ready.eq(axi.ar.ready),
+            axi.ar.addr.eq(axi_lite.ar.addr),
+            axi.ar.burst.eq(burst_type),
+            axi.ar.len.eq(0),
+            axi.ar.size.eq(burst_size),
+            axi.ar.lock.eq(0),
+            axi.ar.prot.eq(0),
+            axi.ar.cache.eq(0b0011),
+            axi.ar.qos.eq(0),
+            axi.ar.id.eq(read_id),
+
+            axi_lite.r.valid.eq(axi.r.valid),
+            axi_lite.r.resp.eq(axi.r.resp),
+            axi_lite.r.data.eq(axi.r.data),
+            axi.r.ready.eq(axi_lite.r.ready),
+        ]
 
 # IOs ----------------------------------------------------------------------------------------------
 
@@ -147,8 +206,13 @@ class BaseSoC(SoCCore):
 
                 wb_wider = wishbone.Interface(data_width=wb_hbm.data_width, adr_width=37 - 5)
                 self.comb += wb_wider.connect(wb_hbm)
+
+                axi_lite_hbm = axi.AXILiteInterface(data_width=axi_hbm.data_width, address_width=axi_hbm.address_width)
                 self.submodules.wb2axi = axi.Wishbone2AXILite(
-                    wb_wider, axi_hbm, base_address=self.bus.regions["main_ram"].origin)
+                    wb_wider, axi_lite_hbm, base_address=self.bus.regions["main_ram"].origin)
+
+                # fixed burst not supported by AXI HBM IP
+                self.submodules.axil2axi = AXILite2AXI(axi_lite_hbm, axi_hbm, burst_type="INCR")
 
                 self.submodules.wb_cpu_debug = BusCSRDebug(
                     description = {
@@ -177,12 +241,6 @@ class BaseSoC(SoCCore):
                 wb_pipe = wb2axi.WishbonePipelined(data_width=256, adr_width=32)
                 self.submodules.wbc2wbp = wb2axi.WishboneClassic2Pipeline(wb_hbm, wb_pipe)
                 self.wbc2wbp.add_sources(platform)
-
-                #  # AXI with address_width expected by wb2axi
-                #  conv_axi = axi.AXIInterface(data_width=axi_hbm.data_width, address_width=32,
-                #                          id_width=axi_hbm.id_width)
-                #  for channel in ["aw", "w", "b", "ar", "r"]:
-                #      self.comb += getattr(conv_axi, channel).connect(getattr(axi_hbm, channel))
 
                 self.submodules.wb2axi = wb2axi.WishbonePipelined2AXI(
                     wb_pipe, axi_hbm, base_address=self.bus.regions["main_ram"].origin)
